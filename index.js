c4b56bb8cbbbd0db3a94456f0890d01cbfd9afcc
Ted Pedersen <tedpedersen@me.com>
2020-07-21 16:21:22 -0700

    adding comments

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const util = require('util');
 const { restoreDefaultPrompts } = require('inquirer');
 
+//create the md file after questions
 const writeFile = util.promisify(fs.writeFile);
 
+//intro message
 console.log('Hi, welcome to the readme generator!');
 
+//user questions
 function promptUser() {
 
     return inquirer.prompt([
@@ -70,11 +73,14 @@ function promptUser() {
     ]);
 } 
 
+//generate the markdown
 const generateReadMe = (response) => {
     var today = new Date();
     var year = today.getFullYear();
+
+    //text for the license
     if(response.license == "MIT"){
-            var licensetxt = 
+    var licensetxt = 
 `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -98,7 +104,8 @@ limitations under the License.`
 `Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`
-};
+}; 
+    //markdown template
     return `# ${response.title}
 ### License:
 ![License](https://img.shields.io/badge/License-${response.license}-blue.svg "License Badge")
@@ -145,7 +152,8 @@ Contact me with any questions:
 
 Email Me: ${response.email}`;
 }
-  
+
+//wait for the answers, then create the markdown
 async function init() {
     try {
         const response = await promptUser();
